d70e1c3a13b52cc2a07b6026bc7437505c32045b
developOnion <smeyonion.nou@gmail.com>
2026-02-01 20:09:16 +0700

    init

---
+++ src/test/java/com/oop/ride_booking/RideBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oop.ride_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
